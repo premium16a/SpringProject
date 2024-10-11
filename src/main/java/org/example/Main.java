22a064548382486dc4df0505768b90a48cf32d8f
tanveer.ahmad <premium16a@gmail.com>
2024-10-11 16:24:10 +0100

    This is my first commit on Git for this project

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,14 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        int i;
+        for(i=0; i< 10; i++)
+        {
+            i=i+1;
+        }
+        System.out.println("The value of I after the above loop is: "+i);
+    }
+
+}
