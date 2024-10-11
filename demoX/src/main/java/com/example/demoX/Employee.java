55cad58c48586f34fb991b89fe82747bd7512d2f
tanveer.ahmad <premium16a@gmail.com>
2024-10-11 11:37:00 +0100

    Spring boot first commit

---
+++ demoX/src/main/java/com/example/demoX/Employee.java
@@ -0,0 +1,19 @@
+package com.example.demoX;
+
+public class Employee {
+        public String name;
+        public String phone;
+        public int age;
+
+        public Employee(String name, String phone, int age)
+        {
+            this.name = name;
+            this.phone = phone;
+            this.age = age;
+        }
+
+        public Employee()
+        {
+
+        }
+}
